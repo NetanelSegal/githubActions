6498b26fe281249262418869456a5ea853b1d9e8
Netanel Segal <segal.netanel4@gmail.com>
2023-12-07 14:25:12 +0200

    node file working

--- app.js
+++
@@ -1,7 +0,0 @@
-console.log("hellow world");
-const urlParams = new URLSearchParams(window.location.href);
-// for (const key of urlParams.keys()) {
-//     console.log(key);
-//     console.log(urlParams.get(key));
-// }
-console.log([...urlParams]);
---
+++ interact_with_webpage.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const browser = await puppeteer.launch({ headless: "new" });
+    const page = await browser.newPage();
+
+    // Define the URL with parameters
+    const url = 'http://127.0.0.1:5500/web_page_code/index.html?num1=12&num2=123'; // Replace with your actual URL and parameters
+
+    // Navigate to the web page with parameters
+    await page.goto(url);
+
+    // Wait for a moment to ensure the page has loaded
+    await page.waitForTimeout(2000); // Adjust the timeout as needed
+
+    // Extract the result from the web page
+    const result = await page.$eval('#result', (element) => element.textContent);
+
+    console.log('Result:', result);
+
+    // Close the browser
+    await browser.close();
+
+    // Exit with an error if the result is not as expected
+    if (result !== 'Match') {
+        console.error('Error: Results do not match.');
+        process.exit(1);
+    }
+})();
