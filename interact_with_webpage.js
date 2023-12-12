6fac5e1f7cbed4b352953671c0f54977f755065b
NetanelSegal <segal.netanel4@gmail.com>
2023-12-12 15:03:43 +0200

    newPage for every url and getting result

--- interact_with_webpage.js
+++ interact_with_webpage.js
@@ -6,10 +6,13 @@ const { getUrlsFromCommentsArray, getInitialScriptFromHtmlFile } = require("./ut
 const directoryPath = './examples';
 
 const getUrlsForCheckingFromFiles = async (directoryPath) => {
-    let urls = []
+
     try {
         const files = await fs.readdir(directoryPath);
         const htmlFiles = files.filter(file => path.extname(file).toLowerCase() === '.html');
+
+        const browser = await puppeteer.launch({ headless: "new" });
+
         await Promise.all(htmlFiles.map(async (htmlFile) => {
             try {
                 const filePath = path.join(__dirname, 'examples', htmlFile);
@@ -21,54 +24,49 @@ const getUrlsForCheckingFromFiles = async (directoryPath) => {
 
                 const initialScript = await getInitialScriptFromHtmlFile(filePath);
 
-                const url = await `https://check-smartscript-page.glitch.me?${new URLSearchParams({
+                const url = `http://127.0.0.1:5500?${new URLSearchParams({
                     initialScript: encodeURIComponent(initialScript),
                     expectedOutputUrl: encodeURIComponent(expectedOutputUrl),
                 }) + "&" + paramsObject}`;
 
                 if (url) {
-                    urls.push(url)
+                    await checkResultForURL(htmlFile, url, browser)
                 }
             } catch (error) {
                 console.log(`Error ${htmlFile}: ${error} `);
             }
         }));
+
+        browser.close()
     } catch (error) {
         console.error('Error reading directory:', error);
     }
-    return urls
 }
 
 (async () => {
-    const urls = await getUrlsForCheckingFromFiles(directoryPath)
+    const urls = await getUrlsForCheckingFromFiles(directoryPath);
+
 })()
 
 
-const checkResultForURL = async (url, browser) => {
-    const page = await browser.newPage();
+const checkResultForURL = async (htmlFile, url, browser) => {
+    try {
+        const page = await browser.newPage();
 
-    await page.goto(url);
-    const result = await page.$eval('#smartscript-result', (element) => element.textContent);
-    return result
+        await page.goto(url, {
+            waitUntil: "networkidle2",
+        })
+
+        const result = await page.evaluate(() => document.getElementById("smartscript-result").textContent);
+        if (result == "false") {
+            console.log("result: " + result);
+
+            console.log("htmlFile: " + htmlFile);
+            console.log("url: " + url);
+            console.log("************************************");
+        }
+
+    } catch (error) {
+        console.error("Error: " + error);
+    }
 }
-
-// (async () => {
-//     const browser = await puppeteer.launch({ headless: "new" });
-
-//     const page = await browser.newPage();
-
-
-//     await page.goto(url);
-
-//     await page.waitForTimeout(2000); // Adjust the timeout as needed
-
-//     const result = await page.$eval('#result', (element) => element.textContent);
-
-//     console.log('Result:', result);
-
-//     await browser.close();
-//     if (result !== 'Match') {
-//         console.error('Error: Results do not match.');
-//         process.exit(1);
-//     }
-// })();
